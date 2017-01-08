9dbec9f8c763ee9ada7fd2c80e50a8b3d8b7ae7f
juleasGoullee <julesgoullee@gmail.com>
2017-01-08 22:07:31 +0100

    order fetchOffers

--- modules/fetcherOffers.js
+++ modules/fetcherOffers.js
@@ -4,7 +4,7 @@ const server = new Stellar.Server(HORIZON_ENDPOINT);
 
 module.exports = async function fetchOffers(account){
 
-  const resOffers = await server.offers('accounts', account.id).call();
+  const resOffers = await server.offers('accounts', account.id).order('asc').call();
 
   return Array.isArray(resOffers.records) ? resOffers.records : [];
 
